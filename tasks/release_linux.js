398bc8c2b036b0ebe47eed4d4e4a73e9d563067a
Charles Lehner <cel@celehner.com>
2016-02-19 23:03:48 -0500

    Remove default app from runtime

--- tasks/release_linux.js
+++ tasks/release_linux.js
@@ -35,6 +35,10 @@ var copyRuntime = function () {
     return projectDir.copyAsync('node_modules/electron-prebuilt/dist', readyAppDir.path(), { overwrite: true });
 };
 
+var cleanupRuntime = function () {
+    return readyAppDir.removeAsync('resources/default_app');
+};
+
 var packageApp = function () {
     var deferred = Q.defer();
 
@@ -135,6 +139,7 @@ var cleanClutter = function () {
 module.exports = function () {
     return init()
     .then(copyRuntime)
+    .then(cleanupRuntime)
     .then(packageApp)
     .then(finalize)
     .then(renameApp)
